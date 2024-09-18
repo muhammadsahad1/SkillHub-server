e6c6986ee0c92ccf5befd61ceba621cf8a50cc06
muhammad sahad <muhammadsahad2022@gmail.com>
2024-09-18 17:57:14 +0530

    fix : elastic search fixing

--- src/framework/elasticsearch/elasticsearchClient.ts
+++ src/framework/elasticsearch/elasticsearchClient.ts
@@ -1,11 +1,14 @@
-import { Client } from '@elastic/elasticsearch'
+import { Client } from '@elastic/elasticsearch';
 
 const client = new Client({
   node: 'https://justingeorge.site:9200',
-  auth : {
-    username : 'elastic',
-    password : process.env.ELASTICSEARCH_PASSWORD,
+  auth: {
+    username: 'elastic',
+    password: 'B_giGunk5pVra*U1NGop', // Use your actual Elasticsearch password
+  },
+  tls: {
+    rejectUnauthorized: false // Only use this for testing. Remove in production!
   }
-})
+});
 
-export default client
+export default client;
