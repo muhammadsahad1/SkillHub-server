2aad047399f8b2d93c769b497ef3b07c4f32e1b1
muhammad sahad <muhammadsahad2022@gmail.com>
2024-09-20 18:11:59 +0530

    fix : removing unnecessary api calles

--- src/framework/webServer/routes/adminRoute.ts
+++ src/framework/webServer/routes/adminRoute.ts
@@ -6,7 +6,7 @@ import { isAdminAuthenticate } from "../middleware/adminAuth";
 
 export function adminRoute(route: Route): Route {
   route.post("/adminLogin", (req: Req, res: Res, next: Next) => adminController.adminLogin(req, res, next));
-  route.get("/users", isAdminAuthenticate, (req: Req, res: Res, next: Next) => adminController.getUsers(req, res, next));
+  route.get("/users", (req: Req, res: Res, next: Next) => adminController.getUsers(req, res, next));
   route.post("/blockUser",isAdminAuthenticate, (req: Req, res: Res, next: Next) => adminController.blockUser(req, res, next));
   route.get('/verification-request',isAdminAuthenticate,(req : Req, res : Res ,next : Next) => adminController.getVerificationRequests(req,res,next))
   route.post('/changeVerifyStatus',isAdminAuthenticate,(req : Req, res : Res ,next : Next) => adminController.changeVerifyStatus(req,res,next))
@@ -14,7 +14,7 @@ export function adminRoute(route: Route): Route {
   route.post('/changeEventsStatus',isAdminAuthenticate,(req: Req, res: Res, next: Next) => adminController.changeEventStatus(req, res, next));
   route.get('/reports',isAdminAuthenticate, (req: Req, res: Res, next: Next) => adminController.getReports(req, res, next));
   route.post('/action',isAdminAuthenticate, (req: Req, res: Res, next: Next) => adminController.reportAction(req, res, next));
-  route.get('/dashboard',isAdminAuthenticate, (req: Req, res: Res, next: Next) => adminController.dasboardData(req, res, next));
+  route.get('/dashboard', (req: Req, res: Res, next: Next) => adminController.dasboardData(req, res, next));
 
   route.post("/adminLogout",isAdminAuthenticate, (req: Req, res: Res, next: Next) => adminController.logout(req, res, next));
 
