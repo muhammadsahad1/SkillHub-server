b5e4ba9dc607e119f3e5041c1dd45b120e1e8732
muhammad sahad <muhammadsahad2022@gmail.com>
2024-09-20 11:20:28 +0530

    fix : change elasticsearch port

--- src/framework/elasticsearch/elasticsearchClient.ts
+++ src/framework/elasticsearch/elasticsearchClient.ts
@@ -4,10 +4,9 @@ const client = new Client({
   node: "http://localhost:9200",
   auth: {
     username: "elastic",
-    password: "elastic", // Use the new password here
+    password: "YN8Bj5DK_Ud6xMDSJpq9", // Use the new password here
   },
 });
 
 export default client;
 
-// YN8Bj5DK_Ud6xMDSJpq9
