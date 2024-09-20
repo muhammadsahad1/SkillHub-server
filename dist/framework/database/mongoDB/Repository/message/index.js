14b0432766843c09876101eddf35a81b0c7f237e
muhammad sahad <muhammadsahad2022@gmail.com>
2024-09-20 10:10:41 +0530

    fix : latests change in js extenstion

--- dist/framework/database/mongoDB/Repository/message/index.js
+++ dist/framework/database/mongoDB/Repository/message/index.js
@@ -1,6 +1,6 @@
 import { getChat } from "./getMessages.js";
 import { sendMessage } from "./sendMessage.js";
 import { getConversationsUsers } from "./getConversationsUsers.js";
-import { markMessagesAsRead } from './markMessagesAsRead.js';
+import { markMessagesAsRead } from './markMessagesAsRead .js';
 import { sendImage } from "./sendImage.js";
 export { sendImage, sendMessage, getChat, getConversationsUsers, markMessagesAsRead };
