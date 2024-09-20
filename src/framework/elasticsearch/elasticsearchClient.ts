d13cdc4ca611dc856b7b2800925564dc4a1ca839
muhammad sahad <muhammadsahad2022@gmail.com>
2024-09-20 11:32:00 +0530

    fix : change elasticsearch port

--- src/framework/elasticsearch/elasticsearchClient.ts
+++ src/framework/elasticsearch/elasticsearchClient.ts
@@ -1,12 +1,14 @@
 import { Client } from "@elastic/elasticsearch";
 
 const client = new Client({
-  node: "http://localhost:9200",
+  node: "https://localhost:9200",
   auth: {
     username: "elastic",
-    password: "YN8Bj5DK_Ud6xMDSJpq9", // Use the new password here
+    password: "YN8Bj5DK_Ud6xMDSJpq9",
   },
+  tls: {
+    rejectUnauthorized: false // Only use this for self-signed certificates
+  }
 });
 
 export default client;
-
