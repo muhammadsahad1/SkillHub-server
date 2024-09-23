d69d7ac9fed86d0abf4248e221696ea983316c20
muhammad sahad <muhammadsahad2022@gmail.com>
2024-09-23 15:27:20 +0530

    fix : sorted the lists in admin sid e

--- dist/framework/database/mongoDB/Repository/admin/getUsers.js
+++ dist/framework/database/mongoDB/Repository/admin/getUsers.js
@@ -1,10 +1,11 @@
 export const getUsers = async (userModels) => {
-    try {
-        const users = await userModels.find({ role: { $ne: "admin" } });
-        return users;
-    }
-    catch (error) {
-        console.error("Error updating profile:", error);
-        return undefined; // Handle error as needed
-    }
+  try {
+    const users = await userModels
+      .find({ role: { $ne: "admin" } })
+      .sort({ createdAt: -1 });
+    return users;
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    return undefined; // Handle error as needed
+  }
 };
--- dist/framework/database/mongoDB/Repository/admin/getVerificationRequests.js
+++ dist/framework/database/mongoDB/Repository/admin/getVerificationRequests.js
@@ -1,10 +1,11 @@
 export const getVerificationRequests = async (verificationRequestsModel) => {
-    try {
-        const result = await verificationRequestsModel.find({});
-        return result;
-    }
-    catch (error) {
-        console.error("Error updating profile:", error);
-        return undefined; // Handle error as needed
-    }
+  try {
+    const result = await verificationRequestsModel
+      .find({})
+      .sort({ createdAt: -1 });
+    return result;
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    return undefined; // Handle error as needed
+  }
 };
