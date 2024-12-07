040c9102fbddfe001ea94a27d34278c7c807b072
mu_sahad <muhammadsahad2022@gmail.com>
2024-12-07 15:19:58 +0530

    isuees my

--- src/framework/webServer/config/db.ts
+++ src/framework/webServer/config/db.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const mongoURL: string = process.env.MONGO_URL || '';
+console.log("Mongo URL from .env file:", process.env.MONGO_URL);
 
 if (!mongoURL) {
   console.error('MongoDB URL is not defined. Check your .env file.');
